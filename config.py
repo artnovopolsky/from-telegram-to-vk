217b1337e962dddf8a737f3c3c60704689523c98
Artyom Novopolsky <64012206+artnovopolsky@users.noreply.github.com>
2021-04-27 00:13:48 +0300

    Updated config.py

--- config.py
+++ config.py
@@ -1,14 +1,6 @@
-import os
-from datetime import datetime
-
 TG_API_ID = # must be integer
 TG_API_HASH = ''
-CHANNEL_NAME = ''
+TG_CHAT_ID = # must be integer
 
 VK_GROUP_TOKEN = ''
 VK_GROUP_ID = ''
-
-
-def get_unique_filename():
-    media_folder_path = os.path.join(os.path.abspath(''), 'media', datetime.now().strftime("%d-%m-%Y_%H:%M:%S"))
-    return media_folder_path
